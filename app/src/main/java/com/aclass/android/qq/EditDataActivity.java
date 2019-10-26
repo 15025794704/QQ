44f5f295c1a5c714654b4ba17c8a35db26d6b076
15025794704 <56956865+15025794704@users.noreply.github.com>
2019-10-26 11:12:27 +0800

    吴志维的界面内容

---
+++ app/src/main/java/com/aclass/android/qq/EditDataActivity.java
@@ -0,0 +1,13 @@
+package com.aclass.android.qq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_data);
+    }
+}
---
+++ app/src/main/java/com/aclass/android/qq/VideoWindowActivity.java
@@ -0,0 +1,13 @@
+package com.aclass.android.qq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoWindowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window_video);
+    }
+}
