e4fc89aad88589d0f0a5c5c1202eae8cb7f3855a
cliffii <cliffordliu@qq.com>
2019-11-03 22:28:21 +0800

    刘飞

---
+++ app/src/main/java/com/aclass/android/qq/AddFriendActivity.java
@@ -0,0 +1,16 @@
+package com.aclass.android.qq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 添加好友，填写验证消息
+ */
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+}
---
+++ app/src/main/java/com/aclass/android/qq/NewFriendActivity.java
@@ -0,0 +1,16 @@
+package com.aclass.android.qq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 搜索 QQ 好友，搜索群组
+ */
+public class NewFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_friend);
+    }
+}
