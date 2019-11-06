082ce54eb60560c1e05c462661795cb322cc8995
cliffii <cliffordliu@qq.com>
2019-11-06 16:15:08 +0800

    实现 Parcelable by 刘飞

--- app/src/main/java/com/aclass/android/qq/entity/Member.java
+++ app/src/main/java/com/aclass/android/qq/entity/Member.java
@@ -1,8 +1,11 @@
 package com.aclass.android.qq.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
-public class Member extends Entity implements Serializable{
+public class Member extends Entity implements Serializable, Parcelable {
 	/**
 	 * 
 	 */
@@ -13,6 +16,30 @@ public class Member extends Entity implements Serializable{
 	private int isHide;//屏蔽
 	private int isDisturb;//免打扰
 	private String niCheng;//成员昵称
+
+	public Member(){}
+
+	protected Member(Parcel in) {
+		qunID = in.readString();
+		memberQQ = in.readString();
+		isTop = in.readInt();
+		isHide = in.readInt();
+		isDisturb = in.readInt();
+		niCheng = in.readString();
+	}
+
+	public static final Creator<Member> CREATOR = new Creator<Member>() {
+		@Override
+		public Member createFromParcel(Parcel in) {
+			return new Member(in);
+		}
+
+		@Override
+		public Member[] newArray(int size) {
+			return new Member[size];
+		}
+	};
+
 	public String getQunID() {
 		return qunID;
 	}
@@ -55,6 +82,20 @@ public class Member extends Entity implements Serializable{
 	public void setMemberQQ(String memberQQ) {
 		this.memberQQ = memberQQ;
 	}
-	
-	
+
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(qunID);
+		dest.writeString(memberQQ);
+		dest.writeInt(isTop);
+		dest.writeInt(isHide);
+		dest.writeInt(isDisturb);
+		dest.writeString(niCheng);
+	}
 }
