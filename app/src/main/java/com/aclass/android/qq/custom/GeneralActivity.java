eb991525289fb76bdc8b66a40abaf64e12024c84
cliffii <cliffordliu@qq.com>
2019-11-10 16:07:04 +0800

    修复 action bar 问题 by 刘飞

--- app/src/main/java/com/aclass/android/qq/custom/GeneralActivity.java
+++ app/src/main/java/com/aclass/android/qq/custom/GeneralActivity.java
@@ -3,6 +3,7 @@ package com.aclass.android.qq.custom;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,9 @@ public abstract class GeneralActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 隐藏 action bar，临时修补措施 // TODO: 11/10/2019 修改 App theme 并移除这几句话
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
         applyEdgeToEdge();
     }
 
