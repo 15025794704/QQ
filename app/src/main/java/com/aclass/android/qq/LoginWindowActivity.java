cb3f0696bc45dceed0c5de454cb5f3efb476016a
xieyan <2401558728@qq.com>
2019-11-10 23:15:14 +0800

    谢燕

--- app/src/main/java/com/aclass/android/qq/LoginWindowActivity.java
+++ app/src/main/java/com/aclass/android/qq/LoginWindowActivity.java
@@ -1,6 +1,7 @@
 package com.aclass.android.qq;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,9 @@ public class LoginWindowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_window_login);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+
         registerButton= (Button) findViewById(R.id.register);
         //跳转到注册界面
         registerButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/aclass/android/qq/StartWindowActivity.java
+++ app/src/main/java/com/aclass/android/qq/StartWindowActivity.java
@@ -3,6 +3,7 @@ package com.aclass.android.qq;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -15,19 +16,22 @@ public class StartWindowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_window);
-        imageView= (ImageView) findViewById(R.id.img_show);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+
           animationDrawable= (AnimationDrawable) imageView.getBackground();
           animationDrawable.start();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                imageView= (ImageView) findViewById(R.id.img_show);
 
                 Intent intent=new Intent(StartWindowActivity.this,LoginWindowActivity.class);//从一个界面跳转到另外一个界面
                 startActivity(intent);
                 finish();
 
             }
-        },3000);
+        },6000);
 
     }
 }
