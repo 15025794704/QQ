d34f1d86d20b2619e27ec1de2a71b081ccb83b80
xieyan <2401558728@qq.com>
2019-11-11 22:39:15 +0800

    谢燕

--- app/src/main/java/com/aclass/android/qq/LoginWindowActivity.java
+++ app/src/main/java/com/aclass/android/qq/LoginWindowActivity.java
@@ -1,6 +1,7 @@
 package com.aclass.android.qq;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.StrictMode;
 import android.provider.Settings;
 import android.support.v7.app.ActionBar;
@@ -12,6 +13,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.aclass.android.qq.entity.Entity;
+import com.aclass.android.qq.entity.User;
+import com.aclass.android.qq.main.MainFragment;
 import com.aclass.android.qq.tools.MyDateBase;
 
 public class LoginWindowActivity extends AppCompatActivity implements View.OnClickListener {
@@ -61,9 +64,9 @@ public class LoginWindowActivity extends AppCompatActivity implements View.OnCli
                 if(!hasFocus){
                     String pwd=login_password.getText().toString().trim();
                     System.out.print("密码:"+pwd);
-                    if(pwd.length()<6)
+                    if(pwd.length()<3)
                     {
-                        Toast.makeText(LoginWindowActivity.this,"密码不得小于6个字符",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginWindowActivity.this,"密码不得小于3个字符",Toast.LENGTH_SHORT).show();
                     }
 
                 }
@@ -108,17 +111,59 @@ public class LoginWindowActivity extends AppCompatActivity implements View.OnCli
         String account=login_account.getText().toString().trim();
         String pwd=login_password.getText().toString().trim();
         boolean isExitQqAccount=myDateBase.isExitqqNum(account);
+
         if(isExitQqAccount)
         {
-            Toast.makeText(LoginWindowActivity.this,"qq账号正确",Toast.LENGTH_SHORT).show();
+           User user=myDateBase.getUser(account);
+            String userPwd=user.getPassword();
+            if(userPwd.equals(pwd))
+            {
+
+                //保存登录状态，在界面保存登录的账号 定义个方法 saveLoginStatus boolean 状态 , account qq账号;
+                saveLoginStatus(true, account);
+                //登录成功后关闭此页面进入主页
+                Intent data = new Intent();
+                //datad.putExtra( ); name , value ;
+                data.putExtra("isLogin", true);
+                //RESULT_OK为Activity系统常量，状态码为-1
+                // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
+                setResult(RESULT_OK, data);
+                //跳转到主界面，登录成功的状态传递到 主页面 中
+                startActivity(new Intent(LoginWindowActivity.this, TestMainActivity.class));
+                //销毁登录界面
+                LoginWindowActivity.this.finish();
+
+
+            }
+            else
+            {
+                Toast.makeText(LoginWindowActivity.this,"密码错误",Toast.LENGTH_SHORT).show();
+            }
 
         }
         else
         {
-            Toast.makeText(LoginWindowActivity.this,"qq账号不存在",Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginWindowActivity.this,"此qq账号不存在",Toast.LENGTH_SHORT).show();
 
         }
 
 
     }
+
+    /*
+    保存登录状态和登录用户名(账号)到SharedPreferences中
+     */
+    private void saveLoginStatus(boolean status,String account) {
+        //loginInfo表示文件名 ,系统自动存成loginInfo.xml  SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        //获取编辑器
+        SharedPreferences.Editor editor = sp.edit();
+        //存入boolean类型的登录状态
+        editor.putBoolean("isLogin", status);
+        //存入登录状态时的用户名
+        editor.putString("loginUserName", account);
+        //提交修改
+        editor.apply();
+    }
+
 }
---
+++ app/src/main/java/com/aclass/android/qq/TestMainActivity.java
@@ -0,0 +1,55 @@
+package com.aclass.android.qq;
+
+import android.app.ListActivity;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class TestMainActivity extends ListActivity {
+     SharedPreferences sharedPreferences;
+    String account;
+    boolean state;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.activity_test_main);*/
+        sharedPreferences=this.getSharedPreferences("loginInfo",MODE_PRIVATE);
+        account=sharedPreferences.getString("loginUserName", null);
+        state=sharedPreferences.getBoolean("isLogin",false);
+
+        //设置适配器
+        /*SimpleAdapter的参数说明
+         * 第一个参数 表示访问整个android应用程序接口，基本上所有的组件都需要
+         * 第二个参数表示生成一个Map(String ,Object)列表选项
+         * 第三个参数表示界面布局的id  表示该文件作为列表项的组件
+         * 第四个参数表示该Map对象的哪些key对应value来生成列表项
+         * 第五个参数表示来填充的组件 Map对象key对应的资源一依次填充组件 顺序有对应关系
+         * 注意的是map对象可以key可以找不到 但组件的必须要有资源填充  因为 找不到key也会返回null 其实就相当于给了一个null资源
+         * */
+
+
+        SimpleAdapter adapter = new SimpleAdapter(this, getData(account,state),R.layout.activity_test_main,new String[]{"name","state"},new int[]{R.id.accountID,R.id.isLoginID});
+      setListAdapter(adapter);
+
+    }
+    //得到数据
+    private List<Map<String, Object>> getData(String account,boolean state){
+        List<Map<String,Object>> datas = new ArrayList<Map<String, Object>>();
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("name", (Object) account);
+        data.put("state",state);
+        datas.add(data);
+        return datas;
+    }
+
+
+
+}
