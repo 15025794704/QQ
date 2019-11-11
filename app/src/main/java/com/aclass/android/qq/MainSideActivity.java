a82014ff09cbb3d3c860b30ed817d2a2744eda86
Mizar1124 <2782300246@qq.com>
2019-11-11 19:54:40 +0800

    陈Mizar的工作

---
+++ app/src/main/java/com/aclass/android/qq/MainSideActivity.java
@@ -0,0 +1,13 @@
+package com.aclass.android.qq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_side);
+    }
+}
