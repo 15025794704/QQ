97055143734e8463e04e788e36897b8528b789b2
Mizar1124 <2782300246@qq.com>
2019-11-20 20:50:48 +0800

    陈Mizar的工作

--- app/src/main/java/com/aclass/android/qq/MyDataActivity.java
+++ app/src/main/java/com/aclass/android/qq/MyDataActivity.java
@@ -2,6 +2,9 @@ package com.aclass.android.qq;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import com.aclass.android.qq.common.MyButtonOperation;
+
 
 /**
  * Created by Adminstrator on 2019/11/19.
@@ -12,5 +15,14 @@ public class MyDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_my);
+
+        Button idBtn=(Button)findViewById(R.id.idBtn);
+        MyButtonOperation.changeButtonBG(this,idBtn,R.drawable.main_side_btn_down,R.drawable.main_side_btn_up);
+
+        Button editBtn=(Button)findViewById(R.id.editBtn);
+        MyButtonOperation.changeButtonBG(this,editBtn,R.drawable.main_side_btn_down,R.drawable.main_side_btn_up);
+
+        Button sendBtn=(Button)findViewById(R.id.sendBtn);
+        MyButtonOperation.changeButtonBG(this,sendBtn,R.drawable.main_side_btn_down,R.drawable.main_side_btn_up);
     }
 }
