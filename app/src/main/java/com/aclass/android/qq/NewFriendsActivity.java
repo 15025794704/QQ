1edc2f9f9a7b9bb44de313e8f2bc208696e76706
Mizar1124 <2782300246@qq.com>
2019-11-22 11:15:24 +0800

    陈Mizar的工作

---
+++ app/src/main/java/com/aclass/android/qq/NewFriendsActivity.java
@@ -0,0 +1,16 @@
+package com.aclass.android.qq;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Adminstrator on 2019/11/21.
+ */
+
+public class NewFriendsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_friends_my);
+    }
+}
