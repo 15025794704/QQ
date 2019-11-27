c187a1ed857b4fbe29aaf0402652fc0317df0800
15025794704 <56956865+15025794704@users.noreply.github.com>
2019-11-27 09:56:15 +0800

    吴志维的更新

--- app/src/main/java/com/aclass/android/qq/CreateGroupActivity.java
+++ app/src/main/java/com/aclass/android/qq/CreateGroupActivity.java
@@ -1,13 +1,36 @@
 package com.aclass.android.qq;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class CreateGroupActivity extends AppCompatActivity {
+import com.aclass.android.qq.custom.GeneralActivity;
+
+public class CreateGroupActivity extends GeneralActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+        init();
+    }
+
+    private void init(){
+        Toolbar toolbar=(Toolbar) findViewById(R.id.activity_create_group_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void consumeInsets(Rect insets) {
+        findViewById(R.id.activity_create_group_toolbar).setPadding(0,insets.top,0,0);
     }
 }
